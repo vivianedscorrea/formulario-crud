e76368dd9958ce3e21559121750afe15569d7cab
FA98VXS <viviane.correa@fiserv.com>
2025-05-26 08:32:27 -0300

    criando o formulario de pesquisa

--- crud-angular-material/src/app/consulta/consulta.component.ts
+++ crud-angular-material/src/app/consulta/consulta.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import {FormsModule } from '@angular/forms'
+import { MatTableModule } from '@angular/material/table'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [ MatInputModule, MatIconModule, MatCardModule, MatTableModule, FormsModule, FlexLayoutModule, MatButtonModule ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
